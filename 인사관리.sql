6b33216e193b8e958adb94ea065df7f1c7c7e1f7
heriong93 <>
2023-12-08 17:54:34 +0900

    sadsa

--- 인사관리.sql
+++ 인사관리.sql
@@ -192,63 +192,81 @@ WHERE (job_id = 'SA_REP'
 OR job_id = 'AD_PRES')
 AND salary > 15000;
 
---1) 
+--1) 급여가 12,000를 넘는 사원의 이름과 급여를 표시하는 질의를 실행하시오.
 SELECT last_name, salary 
 FROM employees 
 WHERE salary > 12000;
 
---2)
+--2)사원 번호가 176인 사원의 이름과 부서 번호를 표시하는 질의를 실행하시오.
 SELECT last_name, department_id
 FROM employees 
 WHERE employee_id = 176;
 
---3) 
+--3) 급여가 5,000에서 12,000 사이에 포함되지 않는 
+  -- 모든 사원의 이름과 급여를 표시하도록 질의를 실행하시오.
 SELECT last_name, salary 
 FROM employees
-WHERE salary BETWEEN 5000 AND 12000;
+WHERE salary NOT BETWEEN 5000 AND 12000;
 
---6)
+--6)급여가 5,000와 12,000 사이이고 부서 번호가 20 또는 50인 사원의 이름과 급여를 나열하도록 질의를 작성하시오. 
+   --열 레이블은 Employee와 Monthly Salary로 각각 지정하시오.
 SELECT last_name AS "Employee", salary AS "Monthly Salary" 
 FROM employees
 WHERE salary BETWEEN 5000 AND 12000
-AND department_id BETWEEN 20 AND 50;
+AND department_id IN (20,50);
 
---7) 
+--7) 2014년에 입사한 모든 사원의 이름과 입사일을 표시하시오.
 SELECT last_name, hire_date
 FROM employees
-WHERE hire_date <= '13/12/31';
+WHERE hire_date BETWEEN '05/01/01' AND '05/12/31';
 
+--7)다른 방법
+SELECT last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '05%';
 
 
 ---------
 SELECT *
 FROM employees;
---8) 
+------------
+
+--8) 관리자가 없는 모든 사원의 이름과 업무를 표시하시오.
 SELECT last_name, job_id
 FROM employees
 WHERE manager_id is null;
 
---10) 
+--10) 이름의 세번째 문자가 a인 모든 사원의 이름을 표시하시오.
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '__a%';
 
---11) 
+--11) 이름에 a와 e가 있는 모든 사원의 이름을 표시하시오.
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%a%'
 AND last_name LIKE '%e%';
 
----12) 
+---12) 업무가 영업 사원(SA_REP) 또는 사무원(ST_CLERK)이면서 
+--급여가 2,500, 3,500, 7,000이 아닌 모든 사원의 이름, 업무 및 급여를 표시하시오.
 SELECT last_name, job_id, salary
 FROM employees
 WHERE (job_id = 'SA_REP'
 OR job_id = 'ST_CLERK')
 AND salary NOT IN (2500,3500,7000);
 
---13) 
+--12)다른 방법
+SELECT last_name, job_id, salary
+FROM employees
+WHERE job_id  IN ('SA_REP', 'ST_CLERK')
+AND salary NOT IN (2500,3500,7000);
+
+--13) 커미션 비율(commission_pct)이 20%인 모든 사원의 이름, 급여 및 커미션을 표시하도록 
+  --  명령문을 작성하여 실행하시오.
 SELECT last_name, salary, commission_pct
 FROM employees
-WHERE commission_pct = (0.2);
+WHERE commission_pct = 0.2;
+
+
 
 
